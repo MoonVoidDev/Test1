4b8f410f4ec6e0be8d7ea5bb6ac49837d850eb14
MoonVoidDev <q1586436934@outlook.com>
2024-10-28 01:06:17 +0800

    1

--- src/startwidget.cpp
+++ src/startwidget.cpp
@@ -16,6 +16,12 @@
 #include "QTimer"
 #include "QToolBar"
 #include "QVBoxLayout"
+#include <format>
+#include "QDialog"
+#include "QDockWidget"
+#include "QStatusBar"
+#include "QMenuBar"
+#include "QTreeWidget"
 
 StartWidget::StartWidget(QWidget* parent)
     : QWidget(parent), ui(new Ui::StartWidget) {
@@ -23,72 +29,14 @@ StartWidget::StartWidget(QWidget* parent)
 
     this->setWindowFlag(Qt::Widget);
 
-    QVBoxLayout* mainvl{ new QVBoxLayout(this) };
+    QVBoxLayout* vlay{ new QVBoxLayout{this} };
+    this->setLayout(vlay);
 
-    QStackedLayout* sl{ new QStackedLayout(mainvl) };
+    QComboBox* cb{ new QComboBox{this} };
+    cb->setPlaceholderText("cnm");
+    vlay->addWidget(cb);
 
-    QWidget* page1w{ new QWidget(this) };
-    QWidget* page2w{ new QWidget(this) };
-    QVBoxLayout* page1vl{ new QVBoxLayout(page1w) };
-    QVBoxLayout* page2vl{ new QVBoxLayout(page2w) };
 
-    QPushButton* b1{ new QPushButton("b1", this) };
-    QPushButton* b2{ new QPushButton("b2", this) };
-    QPushButton* b3{ new QPushButton("b98", this) };
-    QPushButton* b4{ new QPushButton("b97", this) };
-
-    page1vl->addStretch();
-    page1vl->addWidget(b1);
-    page1vl->addWidget(b2);
-    page1vl->addStretch();
-    page2vl->addWidget(b3);
-    page2vl->addWidget(b4);
-    sl->addWidget(page1w);
-    sl->addWidget(page2w);
-
-    QPushButton* nextPage{ new QPushButton("nxt", this) };
-    QPushButton* prePage{ new QPushButton("pre", this) };
-
-    QHBoxLayout* hl{ new QHBoxLayout(nullptr) };
-
-    hl->addStretch(4);
-    hl->addWidget(prePage);
-    hl->addStretch(1);
-    hl->addWidget(nextPage);
-    hl->addStretch(4);
-
-    mainvl->addLayout(hl);
-
-    prePage->setEnabled(false);
-    sl->setCurrentIndex(0);
-
-    connect(nextPage, &QPushButton::clicked, this,
-        [sl, prePage, nextPage]() -> void {
-            if (sl->currentIndex() < sl->count() - 1) {
-                sl->setCurrentIndex(sl->currentIndex() + 1);
-                prePage->setEnabled(true);
-            }
-            if (sl->currentIndex() == sl->count() - 1) {
-                nextPage->setEnabled(false);
-            }
-            else {
-                nextPage->setEnabled(true);
-            }
-        });
-
-    connect(prePage, &QPushButton::clicked, this,
-        [sl, prePage, nextPage]() -> void {
-            if (sl->currentIndex() > 0) {
-                sl->setCurrentIndex(sl->currentIndex() - 1);
-                nextPage->setEnabled(true);
-            }
-            if (sl->currentIndex() == 0) {
-                prePage->setEnabled(false);
-            }
-            else {
-                prePage->setEnabled(true);
-            }
-        });
 
 }
 
